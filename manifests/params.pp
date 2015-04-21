54fc376dae7489c148e99388c861b1a032cf564c
Marcin Witowski <mwitowski@x-formation.com>
2015-04-21 15:57:24 +0200

    Changes default version of client

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class stash_backup_client::params {
   $home_stash      = '/var/atlassian/application-data/stash'
-  $version         = '1.6.0'
+  $version         = '1.8.2'
   $backup_user     = 'backup-stash'
   $backup_password = ''
   $backup_home     = '/backup'
